9cf1fe56b7a45d7b4be636a53d97541ec654bf15
kieranreilly <kieran.reilly1804@gmail.com>
2023-03-10 15:05:03 +0800

    pre adding custom SPM

--- src/main.c
+++ src/main.c
@@ -5,8 +5,25 @@
  */
 
 #include <zephyr/kernel.h>
+#include <modem/nrf_modem_lib.h>
+
+
+
+void nrf_modem_fault_handler(struct nrf_modem_fault_info *fault_info)
+{
+    __NOP();
+    // LOG_INF("Modem fault! Reason: 0x%08X    PC: 0x%08X", pFaultInfo->reason, pFaultInfo->program_counter);
+    k_busy_wait(10000000);
+}
+
+
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	nrf_modem_lib_init(NORMAL_MODE);
+
+	for(;;)
+	{
+		__NOP();
+	}
 }
